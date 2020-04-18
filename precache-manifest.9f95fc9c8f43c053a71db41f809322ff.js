7806a4a7754da71491e83768e3e5327bed4f8fe0
Rakesh <rakesh.sasidharan@gmail.com>
2020-04-18 03:44:55 -0400

    Updates

---
+++ precache-manifest.9f95fc9c8f43c053a71db41f809322ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4085defc5b9819dfec7c51b97d0defba",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "9466df895a91ea0d7aee",
+    "url": "/monsters/static/css/main.f7455705.chunk.css"
+  },
+  {
+    "revision": "2f5f7c7430af51ecdfad",
+    "url": "/monsters/static/js/2.260476f8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.260476f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9466df895a91ea0d7aee",
+    "url": "/monsters/static/js/main.e2082f8a.chunk.js"
+  },
+  {
+    "revision": "50bada5db48adafc125e",
+    "url": "/monsters/static/js/runtime-main.b8a717d5.js"
+  }
+]);
